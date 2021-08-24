dfbd0c0c72d8aaaa03550aff2de1208a3cbb65aa
anfalfadhil <87868348+anfalfadhil@users.noreply.github.com>
2021-08-24 15:48:38 -0400

    alert

--- Project1/the-project/main.js
+++ Project1/the-project/main.js
@@ -32,19 +32,31 @@ console.log("I did it")
 
 //////////////////
 
-
+let secretBoxes =[];
 let globalLettersArray =[];
 let picsArray = ['spaceship/0.jpg', 'spaceship/1.jpg', 'spaceship/1.jpg', 'spaceship/2.jpg', 'spaceship/3.jpg', 'spaceship/4.jpg', 'spaceship/5.jpg', 'spaceship/6.jpg', 'spaceship/7.jpg', 'spaceship/8.jpg', 'spaceship/9.jpg', 'spaceship/10.jpg', 'spaceship/11.jpg', 'spaceship/12.jpg', 'spaceship/13.jpg', 'spaceship/14.jpg', 'spaceship/15.jpg', 'spaceship/16.jpg', 'spaceship/17.jpg' ];
 let img = document.createElement('img');
 document.querySelector("body > div:nth-child(2) > div.spaceship").append(img);
 img.src = 'spaceship/empty.jpg';
 img.setAttribute('class', 'img');
-let secretBoxes =[];
+
+let isInput = true;
+const checkInput = () => {
+   if (document.getElementById("input-box").value === '' ){
+        alert('please enter a word first');
+         isInput = true;
+        
+    } else {
+        appendFuction;
+    }
+}
 
 const appendFuction = () => {
+    checkInput();
 let enteredWord = document.getElementById("input-box").value.toLowerCase();
 let lettersArray = enteredWord.split('');
 globalLettersArray= lettersArray;
+document.getElementById('input-box').value = '';
 document.querySelector("body > div.first-container").style.visibility = "hidden";
 document.querySelector("body > div:nth-child(2)").style.visibility = 'unset';
     for (let i = 0; i< lettersArray.length; i++) {
@@ -54,8 +66,8 @@ document.querySelector("body > div:nth-child(2)").style.visibility = 'unset';
         box.innerText = lettersArray[i];
         secretBoxes.push(box);
         console.log(secretBoxes);
-    }
-}
+    } 
+} 
 
 let enterButton = document.querySelector("body > div.first-container > div > form > button");
 enterButton.onclick = appendFuction;
@@ -72,6 +84,7 @@ let endGame = () => {
         mistakesCounter = 0;
         indexes = [];
         globalLettersArray = [];
+        checkButton.remove();
     }
 }
 
@@ -79,12 +92,13 @@ let checkEnd = () => {
     if ( globalLettersArray.every(element => revealedLetters.includes(element))) {
         console.log(revealedLetters); 
         console.log('p2 wins');
+        checkButton.remove();
         let result = document.createElement('h1');
         result.setAttribute('class', 'result');
         document.querySelector("body > div.third-container").append(result);
         result.innerText = 'Player Two Wins, The spaceman is Gone';
         document.querySelector("body > div.second-container").style.opacity=    '0';
-        document.querySelector("body > div.third-container").style. visibility = 'unset';
+        document.querySelector("body > div.third-container").style.visibility = 'unset';
         endGame();
     } else if (mistakesCounter > 18) {
         let result = document.createElement('h1');
@@ -105,6 +119,8 @@ let checkingFunction = () => {
             console.log('match');
             console.log(secretBoxes);
             checkEnd();
+            // to empty the input box after clicking on the button
+            document.querySelector('.p2-input-box').value = '';
 // https://stackoverflow.com/questions/20798477/how-to-find-index-of-all-occurrences-of-element-in-array
 
                 let getAllMatchingLetters = (array, letter) => {
@@ -133,12 +149,14 @@ const playAgainFunction = () => {
     console.log('play again function');
     endGame();
     document.querySelector("body > div.third-container").style.visibility = 'hidden';
-    document.querySelector("body > div:nth-child(2)").style.visibility = 'unset';
     document.querySelector("body > div.first-container").style.visibility = 'unset';
+    document.querySelector("body > div:nth-child(2)").style.visibility = 'visible';
+  
     enterButton.onclick = appendFuction;
     checkButton.onclick = checkingFunction;
     // document.querySelector("body > div.third-container").style.visibility = 'unset';
 }
+// checkInput();
 checkButton.onclick = checkingFunction;
 const playAgainButton = document.querySelector("body > div.third-container > button");
 playAgainButton.onclick = playAgainFunction;
